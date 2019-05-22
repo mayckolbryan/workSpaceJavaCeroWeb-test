8447606b57bab28e100872588f4e25bdae1d0d36
Bryan Reyes Cruz <mayckolbryan226@gmail.com>
2019-05-22 00:56:26 -0500

    Avance dia 2 - continuando

--- visorapp/visorweb/src/main/java/com/mitocode/controller/specialtyController.java
+++ visorapp/visorweb/src/main/java/com/mitocode/controller/specialtyController.java
@@ -4,12 +4,18 @@
 package com.mitocode.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.event.SelectEvent;
+
+import com.mitocode.model.entity.Specialty;
 import com.mitocode.service.ISpecialtyService;
+import com.mitocode.util.Message;
 
 /**
  * @author mbreyes
@@ -23,4 +29,87 @@ public class specialtyController implements Serializable{
 	
 	@Inject
 	private ISpecialtyService specialtyService;
+
+	private Specialty specialty;
+	private Specialty specialtySelec;
+	private List<Specialty> specialties;
+
+	@PostConstruct
+	public void init() {
+		specialty = new Specialty();
+		specialtySelec = new Specialty();
+		loadSpecialties();
+	}
+
+	public void loadSpecialties() {
+		try {
+			this.specialties = specialtyService.findAll();
+		} catch (Exception e) {
+			Message.messageError("Error Category :" + e.getMessage());
+		}
+	}
+
+	public void saveCategory() {
+		try {
+			if (specialty.getId() != null) {
+
+				Message.messageInfo("Registro actualizado exitosamente");
+				specialtyService.update(specialty);
+			} else {
+				specialtyService.insert(specialty);
+				Message.messageInfo("Registro guardado exitosamente");
+
+			}
+			loadSpecialties();
+			clearForm();
+		} catch (Exception e) {
+			Message.messageError("Error Category :" + e.getStackTrace());
+		}
+	}
+
+	public void editCategory() {
+		try {
+			if (this.specialtySelec != null) {
+				this.specialty = specialtySelec;
+			} else {
+				Message.messageInfo("Debe seleccionar una especialidad");
+			}
+		} catch (Exception e) {
+			Message.messageError("Error Especialidad :" + e.getMessage());
+		}
+
+	}
+
+	public void selectCategory(SelectEvent e) {
+		this.specialtySelec = (Specialty) e.getObject();
+	}
+
+	public void clearForm() {
+		this.specialty = new Specialty();
+		this.specialtySelec = null;
+	}
+
+	public Specialty getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(Specialty specialty) {
+		this.specialty = specialty;
+	}
+
+	public Specialty getSpecialtySelec() {
+		return specialtySelec;
+	}
+
+	public void setSpecialtySelec(Specialty specialtySelec) {
+		this.specialtySelec = specialtySelec;
+	}
+
+	public List<Specialty> getSpecialties() {
+		return specialties;
+	}
+
+	public void setSpecialties(List<Specialty> specialties) {
+		this.specialties = specialties;
+	}
 }
